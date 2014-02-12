753ac5468ac0307a135f237c994d83414b2cfe34
Z Huraibi <zaky.huraibi@hva.nl>
2014-02-12 15:56:18 +0100

    read me

---
+++ Aanwezigheids webapp/include.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+ <link rel="stylesheet" type="text/css" href="css/custom.css"/>
---
+++ Aanwezigheids webapp/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <?php include 'include.php';?>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?> 
+    </body>
+</html>
