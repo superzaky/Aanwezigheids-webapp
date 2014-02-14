6dc7ff0970e0178ed179357ab5b9aeaca3b490a3
superzaky <zaky.huraibi@hva.nl>
2014-02-14 01:21:35 +0100

    added the nav bar that can resize

--- Aanwezigheids webapp/include.php
+++ Aanwezigheids webapp/include.php
@@ -7,5 +7,10 @@
 ?>
  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+ <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+ <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
  <link rel="stylesheet" type="text/css" href="css/custom.css"/>
+ <!-- respond.js need to be put in here and needs to be posted in the header, 
+ if not older versions of IE will see a flash alike content -->
+ <script> src="js/respond.js"</script>
+ 
--- Aanwezigheids webapp/index.php
+++ Aanwezigheids webapp/index.php
@@ -8,9 +8,12 @@
     <head>
         <?php include 'include.php';?>
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PHP Test</title>
+        <title>Home | Aanwezigheidsregistratie</title>
     </head>
     <body>
+        <?php include 'header.php';?>
+        <?php include 'includeJavascript.php';?>
+        
         <?php echo '<p>Hello World</p>'; ?> 
     </body>
 </html>
